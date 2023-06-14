2cd3b5c59f34c9aacb21b758f154ae8b796a119d
vasyillakafy <vasyillakafy@gmail.com>
2023-06-14 21:30:50 +0700

    added components

---
+++ src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../style/kategori.css"
+import Container from 'react-bootstrap/Container';
+
+function Banner({gbr, kategori, desc}) {
+  return (
+    <Container fluid className='gradient1'>
+      <Container>
+        <div className="row align-items-center">
+          <div className="col-sm-4 my-5">
+            <img src={gbr} className="rounded-3" alt="" width="300px" height="300px"/>
+          </div>
+          <div className="col-sm-8 my-4">
+            <h2><strong>{kategori}</strong></h2>
+              {desc}
+          </div>
+        </div>
+      </Container>
+    </Container>
+  );
+}
+
+export default Banner
